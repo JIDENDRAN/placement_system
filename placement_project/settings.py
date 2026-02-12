a8f44defece133555a55cc1d33d76467f7d42436
N Jidendran <jidendran2312003@gmail.com>
2026-02-12 21:02:30 +0530

    Update settings for production env vars and set python version

--- placement_project/settings.py
+++ placement_project/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qh@88hk$0+xfb9((8^4(fx6knga-l7r(qdo9t2b62==2*!fr0*'
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-qh@88hk$0+xfb9((8^4(fx6knga-l7r(qdo9t2b62==2*!fr0*')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
 # Render will set this environment variable
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
